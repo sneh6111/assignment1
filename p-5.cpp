9399632e6fabea647a082c8d36d77f23c2d47b06
sneh6111 <68854311+sneh6111@users.noreply.github.com>
2020-07-27 23:14:54 -0700

    Update p-5.cpp

--- p-5.cpp
+++ p-5.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 int main() 
 {
-    int n, reversed = 0, remainder, original;
+    int n, rev = 0, rem, original;
     cout<<"Enter an integer: ";
     cin>>n;
     original = n;
 
     // reversed integer is stored in reversedN
     while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
+        rem = n % 10;
+        rev = (rev * 10) + remainder;
         n /= 10;
     }
 
     // palindrome if orignalN and reversedN are equal
-    if (original == reversed)
+    if (original == rev)
         cout<<"is a palindrome."<<original;
     else
         cout<<"is not a palindrome."<<original;
